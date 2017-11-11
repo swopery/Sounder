160b5a495c13ac4239ca917aa07b39656a87c72b
Ryan Swope <rswope@middlebury.edu>
2017-11-11 17:38:56 -0500

    Updated Sounder

--- backEnd/migrations/20170507220748_add_int_time_to_matches.js
+++
@@ -1,9 +0,0 @@
-
-exports.up = function(knex, Promise) {
-  return knex.schema.table('matches', function(t){
-    t.integer('matchTimeInt').notNull().defaultTo(0);
-};
-
-exports.down = function(knex, Promise) {
-
-};
